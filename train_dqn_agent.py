db4c40a3a6f6165b313f5cc88f60ed4622c969a6
MiniCharger <dchen110@usc.edu>
2023-11-12 12:46:17 -0800

    Add files via upload

--- train_dqn_agent.py
+++ train_dqn_agent.py
@@ -1,14 +1,23 @@
-from izombie_dqn.dqn_agent_2 import DQNAgent, experienceReplayBuffer_DQN, QNetwork_DQN
 from izombie_env import env as iz_env
-import torch
+
+# from izombie_dqn.dqn_agent_2 import DQNAgent, experienceReplayBuffer_DQN, QNetwork_DQN
+# import torch
+
+# if __name__ == "__main__":
+#     n_iter = 100000
+#     env = iz_env.IZenv(disable_col_6_to_9=True)
+#     nn_name = "test"
+#     buffer = experienceReplayBuffer_DQN(memory_size=100000, burn_in=10000)
+#     net = QNetwork_DQN(env, device="cuda")
+#     agent = DQNAgent(env, net, buffer, batch_size=200)
+#     agent.train(max_episodes=n_iter, evaluate_frequency=5000, evaluate_n_iter=1000)
+#     torch.save(agent.network, nn_name)
+#     agent._save_training_data(nn_name)
+
+
+from izombie_dqn.dqn_agent import DQNAgent
 
 if __name__ == "__main__":
-    n_iter = 100000
-    env = iz_env.IZenv()
-    nn_name = "test"
-    buffer = experienceReplayBuffer_DQN(memory_size=100000, burn_in=10000)
-    net = QNetwork_DQN(env, device="cpu")
-    agent = DQNAgent(env, net, buffer, batch_size=200)
-    agent.train(max_episodes=n_iter, evaluate_frequency=5000, evaluate_n_iter=1000)
-    torch.save(agent.network, nn_name)
-    agent._save_training_data(nn_name)
+    env = iz_env.IZenv(max_step=1_200, disable_col_6_to_9=True)
+    agent = DQNAgent(env=env, device="cuda")
+    agent.train(episodes=100_000)
