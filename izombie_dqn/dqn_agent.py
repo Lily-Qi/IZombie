4e5842701abc3e077fdcfb4495391e403a0c2db4
MiniCharger <dchen110@usc.edu>
2023-11-13 01:20:02 -0800

    Add files via upload

--- izombie_dqn/dqn_agent.py
+++ izombie_dqn/dqn_agent.py
@@ -1,20 +1,25 @@
-import torch.nn as nn
-import torch
-import numpy as np
 from collections import deque
 import random
+from copy import deepcopy
+import os
+
+from torch import nn
+import torch
+import numpy as np
 
 from izombie_env import config
+from izombie_env.simple_env import IZenv
 from .threshold import Threshold
+from .evaluate_agent import evaluate_agent
 
 # model params
 MODEL_NAME = "136,106,76"
 N_HIDDEN_LAYER_NODES = 106
 
 
-class QNetwork_DQN(nn.Module):
-    def __init__(self, device, learning_rate=1e-3):
-        super(QNetwork_DQN, self).__init__()
+class DQNNetwork(nn.Module):
+    def __init__(self, device, learning_rate):
+        super(DQNNetwork, self).__init__()
         self.device = device
 
         self.n_inputs = (
@@ -25,15 +30,12 @@ class QNetwork_DQN(nn.Module):
             + 1  # sun
             + 5  # brain status
         )
-        self.n_outputs = config.ACTION_NO
-        self.actions = np.arange(self.n_outputs)
-        self.learning_rate = learning_rate
 
         # Set up network
         self.network = nn.Sequential(
             nn.Linear(self.n_inputs, N_HIDDEN_LAYER_NODES, bias=True),
             nn.LeakyReLU(),
-            nn.Linear(N_HIDDEN_LAYER_NODES, self.n_outputs, bias=True),
+            nn.Linear(N_HIDDEN_LAYER_NODES, config.ACTION_NO, bias=True),
         )
 
         # Set to GPU if cuda is specified
@@ -41,49 +43,139 @@ class QNetwork_DQN(nn.Module):
             self.network.cuda()
 
         self.optimizer = torch.optim.Adam(
-            filter(lambda p: p.requires_grad, self.parameters()), lr=self.learning_rate
+            filter(lambda p: p.requires_grad, self.parameters()), lr=learning_rate
         )
+
+    def forward(self, x):
+        # Pass the input through the network layers
+        return self.network(x)
 
 
 class DQNAgent:
     def __init__(
         self,
-        env,
-        device="cpu",
+        # model
+        model_name=MODEL_NAME,
+        device="cuda" if torch.cuda.is_available() else "cpu",
+        learning_rate=1e-3,
+        # env
+        step_length=50,  # skip how many game ticks per step (1 game tick = 1cs)
+        max_step_per_ep=300,  # max number of steps allowed in one episode
+        fix_rand=False,  # always generate the same puzzle
+        # hyperparams
         replay_memory_size=50_000,
         min_replay_memory_size=1_000,
-        gamma=0.99,
+        discount=0.99,
         batch_size=64,
         epsilon_length=10_000,
         start_epsilon=1.0,
         epsilon_interpolation="exponential",
         end_epsilon=0.05,
     ):
-        # model params
-        self.env = env
-        self.min_replay_memory_size = min_replay_memory_size
-        self.gamma = gamma
-        self.batch_size = batch_size
-        self.epsilons = Threshold(
-            seq_length=epsilon_length,
-            start_epsilon=start_epsilon,
-            interpolation=epsilon_interpolation,
-            end_epsilon=end_epsilon,
+        self.model_name = model_name
+        self.model = DQNNetwork(device=device, learning_rate=learning_rate)
+        self.target_model = deepcopy(self.model)
+
+        self.load_env(
+            {
+                "step_length": step_length,
+                "max_step": max_step_per_ep,
+                "fix_rand": fix_rand,
+            }
+        )
+        self.load_hyperparams(
+            {
+                "replay_memory": deque(maxlen=replay_memory_size),
+                "min_replay_memory_size": min_replay_memory_size,
+                "discount": discount,
+                "batch_size": batch_size,
+                "epsilons": Threshold(
+                    seq_length=epsilon_length,
+                    start_epsilon=start_epsilon,
+                    interpolation=epsilon_interpolation,
+                    end_epsilon=end_epsilon,
+                ),
+            }
+        )
+        self.load_stats(
+            {
+                "step_count": 0,
+                "episode_count": 0,
+                "rewards": [],
+                "winning_suns": [],
+                "losses": [],
+                "game_results": [],
+                "steps": [],
+            }
         )
 
-        # models and replay memory
-        self.model = QNetwork_DQN(device=device)
-        self.target_model = QNetwork_DQN(device=device)
-        self.replay_memory = deque(maxlen=replay_memory_size)
+    def load_model(self, checkpoint):
+        self.model_name = checkpoint["model_name"]
 
-        # stats
-        self.step_count = 0
-        self.episode_count = 0
-        self.rewards = []
-        self.winning_suns = []
-        self.losses = []
-        self.game_results = []
-        self.steps = []
+        device = checkpoint["device"]
+        self.model.network.load_state_dict(checkpoint["state_dict"])
+        self.model.to(device)
+        self.model.optimizer.load_state_dict(checkpoint["optimizer"])
+        for state in self.model.optimizer.state.values():
+            for k, v in state.items():
+                if isinstance(v, torch.Tensor):
+                    state[k] = v.to(device)
+
+        self.target_model = deepcopy(self.model)
+
+    def load_env(self, checkpoint):
+        self.env = IZenv(
+            step_length=checkpoint["step_length"],
+            max_step=checkpoint["max_step"],
+            fix_rand=checkpoint["fix_rand"],
+        )
+
+    def load_hyperparams(self, checkpoint):
+        self.replay_memory = checkpoint["replay_memory"]
+        self.min_replay_memory_size = checkpoint["min_replay_memory_size"]
+        self.discount = checkpoint["discount"]
+        self.batch_size = checkpoint["batch_size"]
+        self.epsilons = checkpoint["epsilons"]
+
+    def load_stats(self, checkpoint):
+        self.step_count = checkpoint["step_count"]
+        self.episode_count = checkpoint["episode_count"]
+        self.rewards = checkpoint["rewards"]
+        self.winning_suns = checkpoint["winning_suns"]
+        self.losses = checkpoint["losses"]
+        self.game_results = checkpoint["game_results"]
+        self.steps = checkpoint["steps"]
+
+    def load_checkpoint(self, filename):
+        checkpoint = torch.load(filename)
+        self.load_model(checkpoint)
+        self.load_env(checkpoint)
+        self.load_hyperparams(checkpoint)
+        self.load_stats(checkpoint)
+
+    def save_checkpoint(self, filename):
+        checkpoint = {
+            "model_name": self.model_name,
+            "device": self.model.device,
+            "state_dict": self.model.network.state_dict(),
+            "optimizer": self.model.optimizer.state_dict(),
+            "step_length": self.env.step_length,
+            "max_step": self.env.max_step,
+            "fix_rand": self.env.fix_rand,
+            "replay_memory": self.replay_memory,
+            "min_replay_memory_size": self.min_replay_memory_size,
+            "discount": self.discount,
+            "batch_size": self.batch_size,
+            "epsilons": self.epsilons,
+            "step_count": self.step_count,
+            "episode_count": self.episode_count,
+            "rewards": self.rewards,
+            "winning_suns": self.winning_suns,
+            "losses": self.losses,
+            "game_results": self.game_results,
+            "steps": self.steps,
+        }
+        torch.save(checkpoint, filename)
 
     def get_epsilon(self):
         return self.epsilons.epsilon(self.episode_count)
@@ -92,7 +184,6 @@ class DQNAgent:
         with torch.no_grad():
             state_tensor = torch.FloatTensor(state).unsqueeze(0).to(self.model.device)
             q_values = self.model.network(state_tensor).detach()
-
             valid_q_values = q_values[0, valid_actions]
             max_q_index = torch.argmax(valid_q_values).item()
             return valid_actions[max_q_index]
@@ -100,106 +191,128 @@ class DQNAgent:
     def decide_action(self, state, valid_actions):
         if np.random.rand() < self.get_epsilon():
             return np.random.choice(valid_actions)
-        else:
-            return self.get_best_q_action(state, valid_actions)
+        return self.get_best_q_action(state, valid_actions)
+
+    def set_to_trainig_mode(self):
+        self.model.train()
+        self.target_model.train()
+
+    def set_to_eval_mode(self):
+        self.model.eval()
+        self.target_model.eval()
+
+    def take_step(self, state):
+        action = self.decide_action(state, self.env.get_valid_actions())
+
+        reward, next_state, game_status = self.env.step(action)
+        done = game_status != config.GameStatus.CONTINUE
+        self.step_count += 1
+        self.replay_memory.append((state, action, reward, next_state, done))
+
+        self.rewards.append(reward)
+        return next_state, game_status, done
+
+    def update_main_model(self):
+        # Sample a batch of experiences from replay memory
+        minibatch = random.sample(self.replay_memory, self.batch_size)
+        states, actions, rewards, next_states, dones = zip(*minibatch)
+
+        # Convert to tensors
+        states = torch.FloatTensor(np.array(states)).to(self.model.device)
+        actions = torch.LongTensor(np.array(actions)).to(self.model.device)
+        rewards = torch.FloatTensor(np.array(rewards)).to(self.model.device)
+        next_states = torch.FloatTensor(np.array(next_states)).to(self.model.device)
+        dones = torch.FloatTensor(np.array(dones)).to(self.model.device)
+
+        # Compute current Q values
+        current_q_values = (
+            self.model.network(states).gather(1, actions.unsqueeze(1)).squeeze(1)
+        )
+
+        # Compute next Q values using target network
+        next_q_values = self.target_model.network(next_states).max(1)[0]
+        target_q_values = rewards + self.discount * next_q_values * (1 - dones)
+
+        # Compute loss
+        loss = nn.MSELoss()(current_q_values, target_q_values.detach())
+
+        # Optimize the model
+        self.model.optimizer.zero_grad()
+        loss.backward()
+        self.model.optimizer.step()
+
+        self.losses.append(loss.item())
 
     def train(
-        self, episodes, update_main_every=1, update_target_every=1, stats_window=100
+        self,
+        episodes,
+        update_main_every_n_steps=32,
+        update_target_every_n_steps=2000,
+        eval_every_n_episodes=500,
+        stats_window=1000,
     ):
-        self.model.train()  # Set the network to training mode
+        self.set_to_trainig_mode()
+        prev_ep_count = self.episode_count
 
         for episode in range(episodes):
             state = self.env.reset()
-            total_loss = 0
             done = False
 
             while not done:
-                action = self.decide_action(
-                    state=state, valid_actions=self.env.get_valid_actions()
-                )
+                next_state, game_status, done = self.take_step(state)
 
-                reward, next_state, game_status = self.env.step(action)
-                done = game_status != config.GameStatus.CONTINUE
-                self.step_count += 1
-                self.replay_memory.append((state, action, reward, next_state, done))
-
-                self.rewards.append(reward)
                 if done:
                     self.game_results.append(game_status)
                     if game_status == config.GameStatus.WIN:
-                        self.winning_suns.append(self.env._get_sun())
+                        self.winning_suns.append(self.env.get_sun())
 
                 if (
                     len(self.replay_memory) > self.min_replay_memory_size
-                    and self.step_count % update_main_every == 0
+                    and self.step_count % update_main_every_n_steps == 0
                 ):
-                    # Sample a batch of experiences from replay memory
-                    minibatch = random.sample(self.replay_memory, self.batch_size)
-                    states, actions, rewards, next_states, dones = zip(*minibatch)
+                    self.update_main_model()
 
-                    # Convert to tensors
-                    states = torch.FloatTensor(np.array(states)).to(self.model.device)
-                    actions = torch.LongTensor(np.array(actions)).to(self.model.device)
-                    rewards = torch.FloatTensor(np.array(rewards)).to(self.model.device)
-                    next_states = torch.FloatTensor(np.array(next_states)).to(
-                        self.model.device
-                    )
-                    dones = torch.FloatTensor(np.array(dones)).to(self.model.device)
-
-                    # Compute current Q values
-                    current_q_values = (
-                        self.model.network(states)
-                        .gather(1, actions.unsqueeze(1))
-                        .squeeze(1)
-                    )
-
-                    # Compute next Q values using target network
-                    next_q_values = self.target_model.network(next_states).max(1)[0]
-                    target_q_values = rewards + self.gamma * next_q_values * (1 - dones)
-
-                    # Compute loss
-                    loss = nn.MSELoss()(current_q_values, target_q_values.detach())
-
-                    # Optimize the model
-                    self.model.optimizer.zero_grad()
-                    loss.backward()
-                    self.model.optimizer.step()
-
-                    total_loss += loss.item()
-                    self.losses.append(loss.item())
-
-                if self.step_count % update_target_every == 0:
+                if self.step_count % update_target_every_n_steps == 0:
                     self.target_model.load_state_dict(self.model.state_dict())
 
                 state = next_state
 
-            self.steps.append(self.env._step_count)
+            self.steps.append(self.env.step_count)
 
             self.episode_count += 1
 
+            if self.episode_count % eval_every_n_episodes == 0:
+                evaluate_agent(self)
+
             if self.episode_count % 100 == 0:
                 print()
-            game_results_effective_len = min(stats_window, len(self.game_results))
+            win_rate = (
+                sum(
+                    1
+                    for res in self.game_results[-stats_window:]
+                    if res == config.GameStatus.WIN
+                )
+                * 100
+                / min(stats_window, len(self.game_results))
+            )
             print(
-                "\rEp {:d}/{:d}\t ε {:.2f}\t Mean rewards {:.2f}\t Mean losses {:.2f}\t Mean winning sun {:.2f} Mean steps {:.2f} Win {:.2f}%".format(
-                    episode,
-                    episodes,
-                    self.get_epsilon(),
-                    np.mean(self.rewards[-stats_window:]),
-                    np.mean(self.losses[-stats_window:]),
-                    np.mean(self.winning_suns[-stats_window:]),
-                    np.mean(self.steps[-stats_window:]),
-                    sum(
-                        1
-                        for res in self.game_results[-stats_window:]
-                        if res == config.GameStatus.WIN
-                    )
-                    * 100
-                    / game_results_effective_len,
-                ),
+                f"\rEp {prev_ep_count + episode}/{prev_ep_count + episodes} "
+                f"ε {self.get_epsilon():.2f} "
+                f"Mean losses {np.mean(self.losses[-stats_window:]):.2f} "
+                f"Mean winning sun {np.mean(self.winning_suns[-stats_window:]):.2f} "
+                f"Mean steps {np.mean(self.steps[-stats_window:]):.2f} "
+                f" Win {win_rate:.2f}%",
                 end="",
             )
 
-        model_save_dir = f"{MODEL_NAME}.pth"
-        print(f"Training complete! Model has been saved to {model_save_dir}.")
-        torch.save(self.model.state_dict(), model_save_dir)
+        create_folder_if_not_exist("model")
+        filename = f"model/{self.model_name}.pth"
+        self.save_checkpoint(filename)
+        print(f"Training complete! Model has been saved to {filename}.")
+
+
+def create_folder_if_not_exist(folder_name):
+    current_directory = os.getcwd()
+    folder_path = os.path.join(current_directory, folder_name)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
--- izombie_dqn/evaluate_agent.py
+++ izombie_dqn/evaluate_agent.py
@@ -1,47 +1,47 @@
-from izombie_env import config
-from izombie_env import simple_env as iz_env
 from collections import Counter
 
+from izombie_env import config
+from izombie_env import simple_env as iz_env
 
-def evaluate_agent(agent, test_size=100):
-    agent.model.eval()
+
+def evaluate_agent(agent, test_size=10):
+    agent.set_to_eval_mode()
 
     game_results = []
     for _ in range(test_size):
-        env = iz_env.IZenv(fix_rand=True)
+        env = iz_env.IZenv(max_step=agent.env.max_step, fix_rand=True)
         state = env.reset()
 
-        for _ in range(1000):
-            action = agent.get_best_q_action(
-                state=state, valid_actions=env.get_valid_actions()
-            )
+        for _ in range(agent.env.max_step + 1):
+            action = agent.get_best_q_action(state, env.get_valid_actions())
             _, next_state, game_status = env.step(action)
             state = next_state
 
             if game_status != config.GameStatus.CONTINUE:
                 game_results.append(game_status)
                 break
-    
+
     status_counts = Counter(game_results)
     total = len(game_results)
 
+    print()
     for status in config.GameStatus:
         count = status_counts.get(status, 0)
         percentage = (count / total) * 100 if total > 0 else 0
         print(f"{status.name}: Count = {count}, Percentage = {percentage:.2f}%")
+    agent.set_to_trainig_mode()
 
-    
 
-def manually_test_agent(agent, env):
-    agent.model.eval()
+def manually_test_agent(agent):
+    agent.set_to_eval_mode()
+
+    env = iz_env.IZenv(fix_rand=True)
     state = env.reset()
 
     env.print_human_readable_state()
 
     for _ in range(1000):
-        action = agent.get_best_q_action(
-            state=state, valid_actions=env.get_valid_actions()
-        )
+        action = agent.get_best_q_action(state, env.get_valid_actions())
         _, next_state, game_status = env.step(action)
         print(f"ACTION: {action}")
         env.print_human_readable_state()
@@ -52,3 +52,4 @@ def manually_test_agent(agent, env):
 
         _ = input("")
 
+    agent.set_to_trainig_mode()
