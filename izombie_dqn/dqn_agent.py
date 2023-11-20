04c98d32c135e814f144f8e51267eaee24fc2bab
Gary Mao <yunhanm@usc.edu>
2023-11-19 18:22:09 -0800

    commit

--- izombie_dqn/dqn_agent.py
+++ izombie_dqn/dqn_agent.py
@@ -3,71 +3,96 @@ import random
 from copy import deepcopy
 import os
 import datetime
+import zipfile
+# UCB
+import math
+# for profiling
+from io import StringIO
+import cProfile
+import pstats
+import functools
 
 from torch import nn
 import torch
 import numpy as np
 
 from izombie_env import config
-from izombie_env.simple_env import IZenv
-from .threshold import Threshold
+from izombie_env.env import IZenv
+from .epsilons import Epsilons
 from .evaluate_agent import evaluate_agent
 
 # model params
-N_HIDDEN_LAYER_NODES = 84
+N_HIDDEN_LAYER_NODES = 100
 MODEL_NAME = f"{config.STATE_SIZE},{N_HIDDEN_LAYER_NODES},{config.ACTION_SIZE}"
 
 
 class DQNNetwork(nn.Module):
-    def __init__(self, device, learning_rate):
+    def __init__(self, device, learning_rate, dropout_rate=0.5, num_lstm_layers=2):
         super(DQNNetwork, self).__init__()
         self.device = device
-
-        # Set up network
-        self.network = nn.Sequential(
-            nn.Linear(config.STATE_SIZE, N_HIDDEN_LAYER_NODES, bias=True),
-            nn.LeakyReLU(),
-            nn.Linear(N_HIDDEN_LAYER_NODES, config.ACTION_SIZE, bias=True),
-        )
-
-        # Set to GPU if cuda is specified
-        if self.device == "cuda":
-            self.network.cuda()
-
-        self.optimizer = torch.optim.Adam(
-            filter(lambda p: p.requires_grad, self.parameters()), lr=learning_rate
-        )
+        self.lstm = nn.LSTM(input_size=config.STATE_SIZE,
+                            hidden_size=N_HIDDEN_LAYER_NODES,
+                            num_layers=num_lstm_layers,
+                            batch_first=True)
+        self.dropout = nn.Dropout(dropout_rate)
+        self.out = nn.Linear(N_HIDDEN_LAYER_NODES, config.ACTION_SIZE)
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate, weight_decay=1e-5)
 
     def forward(self, x):
-        # Pass the input through the network layers
-        return self.network(x)
+        self.lstm.flatten_parameters()
+        x, _ = self.lstm(x)
+        x = self.dropout(x)
+        x = self.out(x[:, -1, :])
+        return x
+
+
+def profiled(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        profiler = cProfile.Profile()
+        profiler.enable()
+        result = func(*args, **kwargs)
+        profiler.disable()
+        s = StringIO()
+        ps = pstats.Stats(profiler, stream=s).sort_stats("cumulative")
+        ps.print_stats()
+        print(s.getvalue())
+        return result
+
+    return wrapper
 
 
 class DQNAgent:
     def __init__(
-        self,
-        # model
-        model_name=MODEL_NAME,
-        device="cuda" if torch.cuda.is_available() else "cpu",
-        learning_rate=1e-3,
-        # env
-        step_length=50,  # skip how many game ticks per step (1 game tick = 1cs)
-        max_step_per_ep=300,  # max number of steps allowed in one episode
-        fix_rand=False,  # always generate the same puzzle
-        # hyperparams
-        replay_memory_size=50_000,
-        min_replay_memory_size=1_000,
-        discount=0.99,
-        batch_size=64,
-        epsilon_length=10_000,
-        start_epsilon=1.0,
-        epsilon_interpolation="exponential",
-        end_epsilon=0.05,
+            self,
+            # model
+            model_dir="model",
+            model_name=MODEL_NAME,
+            device="cuda" if torch.cuda.is_available() else "cpu",
+            learning_rate=1e-3,
+            # env
+            step_length=50,  # skip how many game ticks per step (1 game tick = 1cs)
+            max_step_per_ep=300,  # max number of steps allowed in one episode
+            fix_rand=False,  # always generate the same puzzle
+            # hyperparams
+            replay_memory_size=50_000,
+            min_replay_memory_size=1_000,
+            discount=0.99,
+            batch_size=64,
+            epsilon_length=10_000,
+            start_epsilon=1.0,
+            epsilon_interpolation="exponential",
+            end_epsilon=0.05,
     ):
         print(f"Using {device} device.")
-        self.model_name = model_name
-        self.model = DQNNetwork(device=device, learning_rate=learning_rate)
+        self.device = torch.device(device)
+        self.model_dir = model_dir
+        self.model_name = f"{model_name}_{get_timestamp()}"
+        self.model = DQNNetwork(device=device, learning_rate=learning_rate).to(device)
         self.target_model = deepcopy(self.model)
+
+        self.action_visit_count = {action: 0 for action in range(config.ACTION_SIZE)}
+        self.action_values = {action: 0 for action in range(config.ACTION_SIZE)}
 
         self.load_env(
             {
@@ -82,7 +107,7 @@ class DQNAgent:
                 "min_replay_memory_size": min_replay_memory_size,
                 "discount": discount,
                 "batch_size": batch_size,
-                "epsilons": Threshold(
+                "epsilons": Epsilons(
                     seq_length=epsilon_length,
                     start_epsilon=start_epsilon,
                     interpolation=epsilon_interpolation,
@@ -102,17 +127,49 @@ class DQNAgent:
             }
         )
 
+    def ucb_action_selection(self, state, valid_actions):
+        total_visit_count = sum(self.action_visit_count[action] for action in valid_actions)
+        log_term = math.log(total_visit_count + 1)
+
+        ucb_values = []
+        for action in valid_actions:
+            action_visit_count = self.action_visit_count[action]
+            if action_visit_count == 0:
+                ucb_values.append(float('inf'))  # Encourage exploration of untried actions
+            else:
+                action_value = self.action_values[action]
+                ucb_values.append(action_value + math.sqrt(2 * log_term / action_visit_count))
+
+        best_action = valid_actions[np.argmax(ucb_values)]
+        self.action_visit_count[best_action] += 1
+        return best_action
+
+    def update_action_values(self, action, reward):
+        if self.action_visit_count[action] > 0:
+            old_value = self.action_values[action]
+            new_value = old_value + (reward - old_value) / self.action_visit_count[action]
+            self.action_values[action] = new_value
+
+    def decide_action(self, state, valid_actions):
+        if np.random.rand() < self.epsilons.get():
+            action = np.random.choice(valid_actions)
+            self.action_visit_count[action] += 1
+            return action
+        return self.ucb_action_selection(state, valid_actions)
+
     def load_model(self, checkpoint):
+        self.model_dir = (
+            checkpoint["model_dir"] if "model_dir" in checkpoint else "model"
+        )
         self.model_name = checkpoint["model_name"]
 
-        device = checkpoint["device"]
         self.model.network.load_state_dict(checkpoint["state_dict"])
-        self.model.to(device)
+        self.model.to(self.device)
         self.model.optimizer.load_state_dict(checkpoint["optimizer"])
         for state in self.model.optimizer.state.values():
             for k, v in state.items():
                 if isinstance(v, torch.Tensor):
-                    state[k] = v.to(device)
+                    state[k] = v.to(self.device)
 
         self.target_model = deepcopy(self.model)
 
@@ -128,7 +185,18 @@ class DQNAgent:
         self.min_replay_memory_size = checkpoint["min_replay_memory_size"]
         self.discount = checkpoint["discount"]
         self.batch_size = checkpoint["batch_size"]
-        self.epsilons = checkpoint["epsilons"]
+
+        from .threshold import Threshold
+
+        epsilons = checkpoint["epsilons"]
+        if isinstance(epsilons, Threshold):
+            epsilons = Epsilons(
+                seq_length=epsilons.seq_length,
+                start_epsilon=epsilons.start_epsilon,
+                end_epsilon=epsilons.end_epsilon,
+            )
+            epsilons.index = checkpoint["epsilon_index"]
+        self.epsilons = epsilons
 
     def load_stats(self, checkpoint):
         self.step_count = checkpoint["step_count"]
@@ -140,16 +208,21 @@ class DQNAgent:
         self.steps = checkpoint["steps"]
 
     def load_checkpoint(self, filename):
-        checkpoint = torch.load(filename)
+        with zipfile.ZipFile(filename, "r") as zipf:
+            zipf.extractall(os.path.dirname(filename))
+        filename = filename[:-4]
+        checkpoint = torch.load(filename, map_location=self.device)
         self.load_model(checkpoint)
         self.load_env(checkpoint)
         self.load_hyperparams(checkpoint)
         self.load_stats(checkpoint)
+        os.remove(filename)
 
     def save_checkpoint(self, filename):
+        assert not os.path.exists(filename)
         checkpoint = {
+            "model_dir": self.model_dir,
             "model_name": self.model_name,
-            "device": self.model.device,
             "state_dict": self.model.network.state_dict(),
             "optimizer": self.model.optimizer.state_dict(),
             "step_length": self.env.step_length,
@@ -169,20 +242,32 @@ class DQNAgent:
             "steps": self.steps,
         }
         torch.save(checkpoint, filename)
+        with zipfile.ZipFile(f"{filename}.zip", "w", zipfile.ZIP_DEFLATED) as zipf:
+            zipf.write(filename, os.path.basename(filename))
+        os.remove(filename)
 
-    def get_epsilon(self):
-        return self.epsilons.epsilon(self.episode_count)
+    def reset_epsilon(
+            self, epsilon_length, start_epsilon, epsilon_interpolation, end_epsilon
+    ):
+        self.epsilons = Epsilons(
+            seq_length=epsilon_length,
+            start_epsilon=start_epsilon,
+            interpolation=epsilon_interpolation,
+            end_epsilon=end_epsilon,
+        )
 
     def get_best_q_action(self, state, valid_actions):
+        self.model.eval()
         with torch.no_grad():
-            state_tensor = torch.FloatTensor(state).unsqueeze(0).to(self.model.device)
-            q_values = self.model.network(state_tensor).detach()
+            state_tensor = torch.FloatTensor(state).unsqueeze(0).unsqueeze(0).to(self.model.device)
+            q_values = self.model(state_tensor).detach()
             valid_q_values = q_values[0, valid_actions]
             max_q_index = torch.argmax(valid_q_values).item()
-            return valid_actions[max_q_index]
+        self.model.train()
+        return valid_actions[max_q_index]
 
     def decide_action(self, state, valid_actions):
-        if np.random.rand() < self.get_epsilon():
+        if np.random.rand() < self.epsilons.get():
             return np.random.choice(valid_actions)
         return self.get_best_q_action(state, valid_actions)
 
@@ -196,8 +281,8 @@ class DQNAgent:
 
     def take_step(self, state, mask):
         action = self.decide_action(state, self.env.get_valid_actions(mask))
-
         reward, next_state, next_mask, game_status = self.env.step(action)
+        self.update_action_values(action, reward)
         done = game_status != config.GameStatus.CONTINUE
         self.step_count += 1
         self.replay_memory.append((state, action, reward, next_state, next_mask, done))
@@ -206,52 +291,44 @@ class DQNAgent:
         return next_state, next_mask, game_status, done
 
     def update_main_model(self):
-        # Sample a batch of experiences from replay memory
         minibatch = random.sample(self.replay_memory, self.batch_size)
         states, actions, rewards, next_states, next_masks, dones = zip(*minibatch)
-
-        # Convert to tensors
-        states = torch.FloatTensor(np.array(states)).to(self.model.device)
+        states = torch.FloatTensor(np.array(states)).unsqueeze(1).to(self.model.device)
         actions = torch.LongTensor(np.array(actions)).to(self.model.device)
         rewards = torch.FloatTensor(np.array(rewards)).to(self.model.device)
-        next_states = torch.FloatTensor(np.array(next_states)).to(self.model.device)
+
+        next_states = torch.FloatTensor(np.array(next_states)).unsqueeze(1).to(self.model.device)
         next_masks = torch.FloatTensor(np.array(next_masks)).to(self.model.device)
         dones = torch.FloatTensor(np.array(dones)).to(self.model.device)
-
-        # Compute current Q values
-        current_q_values = (
-            self.model.network(states).gather(1, actions.unsqueeze(1)).squeeze(1)
-        )
-
-        # Compute next Q values using target network
+        current_q_values = self.model(states).gather(1, actions.unsqueeze(1)).squeeze(1)
+        next_actions = self.model(next_states).argmax(dim=1)
         with torch.no_grad():
-            masked_next_q_values = self.target_model.network(next_states) * next_masks
-        masked_next_q_values[masked_next_q_values == 0] = -1e9
-        next_q_values = masked_next_q_values.max(1)[0]
+            next_q_values = self.target_model(next_states).gather(1, next_actions.unsqueeze(-1)).squeeze(-1)
+
         target_q_values = rewards + self.discount * next_q_values * (1 - dones)
-
-        # Compute loss
-        loss = nn.MSELoss()(current_q_values, target_q_values.detach())
-
-        # Optimize the model
+        loss = nn.SmoothL1Loss()(current_q_values, target_q_values.detach())
         self.model.optimizer.zero_grad()
         loss.backward()
         self.model.optimizer.step()
-
         self.losses.append(loss.item())
 
+    def sync_target_model_with_main(self):
+        self.target_model.load_state_dict(self.model.state_dict())
+
     def train(
-        self,
-        episodes,
-        update_main_every_n_steps=32,
-        update_target_every_n_steps=2000,
-        eval_every_n_episodes=500,
-        eval_max_step=500,
-        stats_window=1000,
+            self,
+            episodes,
+            update_main_every_n_steps,
+            update_target_every_n_steps,
+            evaluate_every_n_episodes,
+            evaluate_test_size,
+            save_checkpoint_every_n_episodes,
+            stats_window,
     ):
         self.set_to_trainig_mode()
-        prev_ep_count = self.episode_count
+        prev_episode_count = self.episode_count
 
+        start_time = datetime.datetime.now()
         for episode in range(episodes):
             self.env.reset()
             state, mask = self.env.get_state_and_mask()
@@ -262,51 +339,78 @@ class DQNAgent:
 
                 if done:
                     self.game_results.append(game_status)
+                    self.steps.append(self.env.step_count)
                     if game_status == config.GameStatus.WIN:
                         self.winning_suns.append(self.env.get_sun())
 
                 if (
-                    len(self.replay_memory) > self.min_replay_memory_size
-                    and self.step_count % update_main_every_n_steps == 0
+                        len(self.replay_memory) > self.min_replay_memory_size
+                        and self.step_count % update_main_every_n_steps == 0
                 ):
                     self.update_main_model()
 
                 if self.step_count % update_target_every_n_steps == 0:
-                    self.target_model.load_state_dict(self.model.state_dict())
+                    self.sync_target_model_with_main()
 
                 state, mask = next_state, next_mask
-
-            self.steps.append(self.env.step_count)
 
             self.episode_count += 1
 
             if self.episode_count % 100 == 0:
-                win_rate = (
-                    sum(
-                        1
-                        for res in self.game_results[-stats_window:]
-                        if res == config.GameStatus.WIN
-                    )
-                    * 100
-                    / min(stats_window, len(self.game_results))
-                )
-                print(
-                    f"Ep {prev_ep_count + episode + 1}/{prev_ep_count + episodes} "
-                    f"ε {self.get_epsilon():.2f} "
-                    f"Mean losses {np.mean(self.losses[-stats_window:]):.2f} "
-                    f"Mean winning sun {np.mean(self.winning_suns[-stats_window:]):.2f} "
-                    f"Mean steps {np.mean(self.steps[-stats_window:]):.2f} "
-                    f" Win {win_rate:.2f}%"
+                self.print_stats(
+                    stats_window,
+                    prev_episode_count + episode + 1,
+                    prev_episode_count + episodes,
+                    (datetime.datetime.now() - start_time).total_seconds()
+                    / (episode + 1),
                 )
 
-            if self.episode_count % eval_every_n_episodes == 0:
-                evaluate_agent(self, max_step=eval_max_step)
+            self.epsilons.next()
 
-        current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-        create_folder_if_not_exist("model")
-        filename = f"model/{self.model_name}_{self.episode_count}_{current_time}.pth"
-        self.save_checkpoint(filename)
-        print(f"Training complete! Model has been saved to {filename}.")
+            if (
+                    evaluate_every_n_episodes is not None
+                    and self.episode_count % evaluate_every_n_episodes == 0
+            ):
+                create_folder_if_not_exist(self.get_model_folder())
+                evaluate_agent(
+                    self,
+                    test_size=evaluate_test_size,
+                    episode_count=self.episode_count,
+                    output_file=f"{self.get_model_folder()}/eval.txt",
+                )
+
+            if (
+                    save_checkpoint_every_n_episodes is not None
+                    and self.episode_count % save_checkpoint_every_n_episodes == 0
+            ):
+                create_folder_if_not_exist(self.get_model_folder())
+                filename = f"{self.get_model_folder()}/model_{self.episode_count}.pth"
+                self.save_checkpoint(filename)
+                print(f"Checkpoint has been saved to {filename}.")
+
+    train_with_profiler = profiled(train)
+
+    def print_stats(self, stats_window, curr_episode_count, total_episode_count, seconds_per_episode):
+        win_rate = (
+                sum(1 for res in self.game_results[-stats_window:] if res == config.GameStatus.WIN) * 100
+                / min(stats_window, len(self.game_results))
+        ) if self.game_results else 0
+        mean_losses = np.mean(self.losses[-stats_window:]) if self.losses else 0
+        mean_winning_suns = np.mean(self.winning_suns[-stats_window:]) if self.winning_suns else 0
+        mean_steps = np.mean(self.steps[-stats_window:]) if self.steps else 0
+
+        print(
+            f"Ep {curr_episode_count}/{total_episode_count} "
+            f"ε {self.epsilons.get():.2f} "
+            f"Mean losses {mean_losses:.2f} "
+            f"Mean winning sun {mean_winning_suns:.2f} "
+            f"Mean steps {mean_steps:.2f} "
+            f"Win {win_rate:.2f}% "
+            f"{int(seconds_per_episode * 10_000)}s/10k ep"
+        )
+
+    def get_model_folder(self):
+        return f"{self.model_dir}/{self.model_name}"
 
 
 def create_folder_if_not_exist(folder_name):
@@ -314,3 +418,7 @@ def create_folder_if_not_exist(folder_name):
     folder_path = os.path.join(current_directory, folder_name)
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
+
+
+def get_timestamp():
+    return datetime.datetime.now().strftime("%Y.%m.%d_%H.%M.%S")
--- train_dqn_agent.py
+++ train_dqn_agent.py
@@ -1,45 +1,33 @@
 import torch
 
-from izombie_env import simple_env as iz_env
 from izombie_dqn.evaluate_agent import manually_test_agent
 from izombie_dqn.dqn_agent import DQNAgent
-
-# from izombie_dqn.dqn_agent_2 import DQNAgent, experienceReplayBuffer_DQN, QNetwork_DQN
-# import torch
-
-# if __name__ == "__main__":
-#     n_iter = 100000
-#     env = iz_env.IZenv(disable_col_6_to_9=True)
-#     nn_name = "test"
-#     buffer = experienceReplayBuffer_DQN(memory_size=100000, burn_in=10000)
-#     net = QNetwork_DQN(env, device="cuda")
-#     agent = DQNAgent(env, net, buffer, batch_size=200)
-#     agent.train(max_episodes=n_iter, evaluate_frequency=5000, evaluate_n_iter=1000)
-#     torch.save(agent.network, nn_name)
-#     agent._save_training_data(nn_name)
-
 
 if __name__ == "__main__":
     agent = DQNAgent(
         device="cuda" if torch.cuda.is_available() else "cpu",
-        learning_rate=1e-3,
+        learning_rate=1e-4,
         step_length=50,
-        max_step_per_ep=500,
+        max_step_per_ep=20000,
         fix_rand=False,
-        replay_memory_size=100_000,
-        min_replay_memory_size=10_000,
-        discount=0.99,
-        batch_size=200,
-        epsilon_length=100_000,
+        replay_memory_size=150000,
+        min_replay_memory_size=20000,
+        discount=0.995,
+        batch_size=128,
+        epsilon_length=200000,
         start_epsilon=1.0,
         epsilon_interpolation="exponential",
-        end_epsilon=0.1,
+        end_epsilon=0.05,
     )
+    # agent.load_checkpoint("model/model_500000.pth.zip")
+    # agent.save_checkpoint("model/model_80001.pth")
     agent.train(
-        episodes=10_000,
-        update_main_every_n_steps=1,
-        update_target_every_n_steps=200,
-        eval_every_n_episodes=1000,
-        stats_window=1_000,
+        episodes=100000,
+        update_main_every_n_steps=32,
+        update_target_every_n_steps=500,
+        evaluate_every_n_episodes=5000,
+        evaluate_test_size=500,
+        save_checkpoint_every_n_episodes=50000,
+        stats_window=1000,
     )
-    manually_test_agent(agent)
+    # manually_test_agent(agent)
